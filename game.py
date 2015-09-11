95e4c825b8d18945d94523db2f8a75538fdc6b2d
jinlee4140 <jinlee4140@gmail.com>
2015-09-10 22:54:41 -0400

    while loop for rock paper scissors

--- game.py
+++ game.py
@@ -6,4 +6,17 @@ choose = ["rock", "paper", "scissor"]
 player_choice = raw_input(">")
 ai_choice = random.choice(choose)
 
+player_score = 0
+ai_score = 0
+
+
+
+# while player_score == 2 or ai_score == 2:
+print "Player Score: %d, Computer Score: %d" % (player_score, ai_score)
+
+
+
+
+
+
 print ai_choice
